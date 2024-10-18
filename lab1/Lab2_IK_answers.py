985c526f37d53ebcd7db254459e2f1f3d5d5e74a
cqf2186863072 <2186863072@qq.com>
2024-10-18 18:27:21 +0800

    Changes

--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -1,5 +1,49 @@
+import math
+
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+
+from lab1.task2_inverse_kinematics import MetaData
+
+
+def solve_ccd_ik(meta_data: MetaData, joint_positions, joint_orientations, target_position, precision, max_interation):
+    # end_part是从end到根骨骼，root_part是root到根骨骼
+    joint_chain, end_part, root_part = meta_data.get_path_from_root_to_end()
+
+    def update_joint(joint_index_in_chain):
+        joint_index = joint_chain[joint_index_in_chain]
+        joint_position = joint_positions[joint_index]
+        joint_orientation = joint_orientations[joint_index]
+        tip_position = joint_positions[joint_chain[-1]]
+
+        # 计算旋转
+        vec_to_tip = tip_position - joint_position
+        vec_to_tip /= np.linalg.norm(vec_to_tip)
+
+        vec_to_target = target_position - joint_position
+        vec_to_target /= np.linalg.norm(vec_to_target)
+
+        angle_to_rotate = np.arccos(np.clip(np.dot(vec_to_target, vec_to_tip), -1.0, 1.0))
+        if angle_to_rotate < precision:
+            return
+
+        rotation_axis = np.cross(vec_to_tip, vec_to_target)
+        rotation_axis /= np.linalg.norm(rotation_axis)
+
+        rotvec = angle_to_rotate * rotation_axis
+        rotation = R.from_rotvec(rotvec)
+
+        # 应用旋转
+        if joint_index in end_part:
+            current_orientation = R.from_quat(joint_orientation)
+            new_orientation = current_orientation * rotation
+
+            joint_orientations[joint_index] = new_orientation
+
+        # 更新所有子joint的位置和朝向
+
+
+
 
 def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, target_pose):
     """
@@ -14,8 +58,7 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
         joint_positions: 计算得到的关节位置，是一个numpy数组，shape为(M, 3)，M为关节数
         joint_orientations: 计算得到的关节朝向，是一个numpy数组，shape为(M, 4)，M为关节数
     """
-    
-    
+
     return joint_positions, joint_orientations
 
 def part2_inverse_kinematics(meta_data, joint_positions, joint_orientations, relative_x, relative_z, target_height):
